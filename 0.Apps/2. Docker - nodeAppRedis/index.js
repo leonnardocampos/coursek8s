c5b94fde59f1e37b1d5023020d50f2ef5f434774
Leonnardo Enes de Campos <leonnardo.campos@radixeng.com.br>
2021-04-09 12:23:10 -0300

    First Commit

---
+++ 0.Apps/2. Docker - nodeAppRedis/index.js
@@ -0,0 +1,23 @@
+const keys = require('./keys');
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  auth_pass: keys.redisPassword
+});
+// client.set('visits', 0);
+
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  })
+});
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081');
+});
