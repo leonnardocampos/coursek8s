d197fced93f10adbf424594b46be8dc39d2b74a9
Leonnardo Enes de Campos <leonnardo.campos@radixeng.com.br>
2021-04-09 16:57:32 -0300

    Sixth travis commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t leoenes/nodeapp:latest -t leoenes/nodeapp:$SHA -f ./0.Apps/2.Docker-nodeAppRedis/Dockerfile ./0.Apps/2.Docker-nodeAppRedis
 docker push leoenes/nodeapp:latest
 docker push leoenes/nodeapp:$SHA
-docker apply -f ./0.Apps/3.Kubernetes-Service_Pod/
+kubectl apply -f ./0.Apps/3.Kubernetes-Service_Pod/
 kubectl set image deployments/client-deployment client=leoenes/nodeapp:$SHA -n coursek8s
